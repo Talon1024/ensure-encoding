d973f0fa709bb13a6d277a0c0b17d4939fa237ed
Kevin Caccamo <kevin@ciinet.org>
2017-03-16 21:57:30 -0400

    Initial commit

---
+++ spec/ensure-encoding-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import EnsureEncodingView from '../lib/ensure-encoding-view';
+
+describe('EnsureEncodingView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
